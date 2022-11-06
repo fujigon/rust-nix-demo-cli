d48d3fc8a00862573033b0207bb385a72aa20599
fujigon <dec12.kaz@gmail.com>
2022-11-07 00:37:01 +0900

    build: a simple logic to get the latest version of rust from github.

--- flake.nix
+++ flake.nix
@@ -1,6 +1,23 @@
 {
   inputs = {
-    nci.url = "github:yusdacra/nix-cargo-integration";
+    nixpkgs.url = "nixpkgs";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+    rust-overlay.inputs.nixpkgs.follows = "nixpkgs";
+    # Documentation and examples:
+    # https://github.com/yusdacra/rust-nix-templater/blob/master/template/flake.nix
+
+    # The latest NCI has a problem of reflecting the default-crate-overrides
+    # https://github.com/NixOS/nixpkgs/blob/master/pkgs/build-support/rust/default-crate-overrides.nix
+    # Thus let's specify the version that was working.
+    nci.url = "github:yusdacra/nix-cargo-integration?rev=29020fc60aaae3f28f5aac441508d2de703d2f67";
+    nci.inputs.nixpkgs.follows = "nixpkgs";
+    nci.inputs.rust-overlay.follows = "rust-overlay";
   };
-  outputs = inputs: inputs.nci.lib.makeOutputs { root = ./.; };
+
+  outputs = inputs:
+    inputs.nci.lib.makeOutputs {
+      root = ./.;
+      # The default buidler (crane) doesn't detect pkg-config
+      builder = "build-rust-package";
+    };
 }
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
-fn main() {
-    println!("Hello, world!");
+use color_eyre::Result;
+use curl::easy::Easy;
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize)]
+struct GitHubRelease {
+    tag_name: String,
+}
+
+fn main() -> Result<()> {
+    let mut buf = Vec::new();
+    let mut easy = Easy::new();
+    easy.url("https://api.github.com/repos/rust-lang/rust/releases/latest")?;
+    easy.follow_location(true)?;
+    easy.useragent("curl")?;
+
+    {
+        let mut transfer = easy.transfer();
+        transfer.write_function(|data| {
+            buf.extend_from_slice(data);
+            Ok(data.len())
+        })?;
+        transfer.perform()?;
+    }
+
+    let response: GitHubRelease = serde_json::from_slice(&buf)?;
+
+    println!("The latest version of rust is {} now.", response.tag_name);
+
+    Ok(())
 }
